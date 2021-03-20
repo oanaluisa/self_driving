0582c77bd9f9475a53390836fe383572a853868e
oana_luisa21@yahoo.com <oana_luisa21@yahoo.com>
2021-03-20 18:37:14 +0200

    add valohai files

--- drive.py
+++ drive.py
@@ -3,7 +3,6 @@ import base64
 from datetime import datetime
 import os
 import shutil
-
 import numpy as np
 import socketio
 import eventlet
@@ -11,10 +10,6 @@ import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
-
-# from keras.models import load_model
-# import h5py
-# from keras import __version__ as keras_version
 import torch
 from torch.autograd import Variable
 import torchvision.transforms as transforms
